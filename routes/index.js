797cc6559eb98e9d7d25dc36fae9ee24fda53d2b
mdowns <michaeljdowns99@gmail.com>
2023-09-21 13:13:32 -0600

    Got Swagger working on Localhost

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 
 router.use('/sodas', require('./sodas'));
+router.use('/', require('./swagger'));
+
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,17 @@
+//RUn: npm run swagger-autogen to update Swagger docs
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Poppin Sodas'
+  },
+  host: 'localHost:8080',
+  schemes: ['http']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
