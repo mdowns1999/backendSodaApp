b0130f352f9b3b7a33d1ed6dbf3735d21277aa56
mdowns <michaeljdowns99@gmail.com>
2023-10-10 13:04:29 -0600

    Fixed Route error

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.use('/sodas', require('./sodas'));
-router.use('/custom', require('./custom-sodas'));
+router.use('/custom', require('./custom'));
 router.use('/orders', require('./orders'));
 router.use('/', require('./swagger'));
 
