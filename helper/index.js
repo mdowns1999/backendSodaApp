3b4891c860f86661aa9d9a25811689bfdfcedc06
mdowns <michaeljdowns99@gmail.com>
2023-10-17 15:33:59 -0600

    Adding Validation.  Test 2

--- helper/index.js
+++ helper/index.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 function validateOrder(order) {
   const JoiSchema = Joi.object({
-    order_id: Joi.string().min(1).max(3).required(),
+    order_id: Joi.number().min(1).max(3).required(),
 
     name: Joi.string().min(1).max(50).required(),
 
-    order_num: Joi.string().min(1).max(6).required(),
+    order_num: Joi.number().min(1).max(6).required(),
 
     // cart: Joi.array().items({
     //     id: Joi.string().min(1).max(3).required(),
