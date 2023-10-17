e22cde6383b44ed996d545a17097f10562935aa9
mdowns <michaeljdowns99@gmail.com>
2023-10-17 15:40:22 -0600

    Test 4: Array with an object item

--- helper/index.js
+++ helper/index.js
@@ -8,7 +8,13 @@ function validateOrder(order) {
 
     order_num: Joi.number().min(1).max(6).required(),
 
-    cart: Joi.array()
+    cart: Joi.array().items({
+        id: Joi.string().min(1).max(3).required(),
+        name: Joi.string().required(),
+        amount: Joi.number().required(),
+        price: Joi.number().required(),
+        size: Joi.string().required()
+    })
 
   }).options({ abortEarly: false });
 
