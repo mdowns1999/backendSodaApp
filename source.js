d434df57e287cdec4532e064d7768bc566b35fb2
mdowns <michaeljdowns99@gmail.com>
2023-10-18 14:55:00 -0600

    Updated Headers

--- source.js
+++ source.js
@@ -10,8 +10,16 @@ app
   .use(bodyParser.json())
   .use(cors())
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
   })
   
 
