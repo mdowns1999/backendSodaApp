ffb25931ceeeb1c1d79ff4e38793d6d96b6deda7
mdowns <michaeljdowns99@gmail.com>
2023-11-13 16:22:14 -0700

    Testing order functio without cart

--- helper/index.js
+++ helper/index.js
@@ -3,18 +3,18 @@ const Joi = require("joi");
 function validateOrder(order) {
   const JoiSchema = Joi.object({
     order_id: Joi.number().min(1).max(1000).required(),
-    name: Joi.string().min(1).max(50).required(),
     order_num: Joi.number().min(1).max(1000).required(),
+    name: Joi.string().min(1).max(50).required(),
     phone: Joi.string().required(),
     notes: Joi.string(),
-    cart: Joi.array().items({
-      id: Joi.string().min(1).max(3).required(),
-      name: Joi.string().required(),
-      amount: Joi.number().required(),
-      price: Joi.number().required(),
-      ingredients: { baseSoda: Joi.string(), ingredients: Joi.array() },
-      size: Joi.number().required(),
-    }),
+    // cart: Joi.array().items({
+    //   id: Joi.string().min(1).max(10).required(),
+    //   name: Joi.string().required(),
+    //   amount: Joi.number().required(),
+    //   price: Joi.number().required(),
+    //   ingredients: { baseSoda: Joi.string(), ingredients: Joi.array() },
+    //   size: Joi.number().required(),
+    // }),
   }).options({ abortEarly: false });
 
   return JoiSchema.validate(order);
