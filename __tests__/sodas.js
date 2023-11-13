052c545e6accc6e4dff961aab36f576a02b22032
mdowns <michaeljdowns99@gmail.com>
2023-11-13 15:33:10 -0700

    Added Backend tests

--- __tests__/sodas.js
+++ __tests__/sodas.js
@@ -27,3 +27,15 @@ describe("Check if this route exsits so we can Post an order", () => {
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
   });
 });
+
+describe("Check if this route exsits so we can Post a review", () => {
+  test("responds to /orders", async () => {
+    const res = await request(app).post("/reviews");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+  });
+
+  test("Responds to request to Get all Reviews", async () => {
+    const res = await request(app).get("/reviews");
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+  });
+});
